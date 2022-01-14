8b35c28c1c40bb174b1f656cbd271a90175bd449
Rabbit101 <70846170+ramune0144@users.noreply.github.com>
2022-01-14 21:27:19 +0700

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-from infi.systray import SysTrayIcon
-def say_hello(systray):
-    print("Hello, World!")
-menu_options = (("Say Hello", None, say_hello),)
-systray = SysTrayIcon("icon.ico", "Example tray icon", menu_options)
-systray.start()
