c2da179f036f1f42e03122e171351d9c58d38a62
naruepon sowchai <naruepon.sao@ku.th>
2022-01-14 21:11:50 +0700

    add TrayIcon

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 from infi.systray import SysTrayIcon
 import keyboard 
-from src import speak,event
-
-
-
- 
+from src import speak,event 
 if __name__ == "__main__":
     menu_options = (("About", None,event.on_about),("How", None,event.on_how))
     systray = SysTrayIcon("./language.ico", "En_Th_OCR", menu_options,on_quit=event.on_end,)
