28af4b22ea38020a49e679f2072da10317f37672
Rabbit101 <70846170+ramune0144@users.noreply.github.com>
2022-01-26 12:21:25 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from infi.systray import SysTrayIcon
-import keyboard 
-from src import speak,event 
+from infi.systray import SysTrayIcon #เป็นการนำเข้าmoduleเพื่อจะเอาไว้ทำTrayIconมุมล่างจอ
+import keyboard #เป็นการนำเข้าmodule เพื่อใช้ keybrod เพื่อจะทำ hotkey
+from src import speak,event #เป็นการนำเข้าโมดูลspeakกับevent speakคือโมดูลสำหรับพูด event คือโมดูลการทำงานเพื่อสร้างtrayIcon
 #!!!!!!!!!!!!!!!!!! This is a debugger !!!!!!!!!!!!!!!!!!
 #                        _oo0oo_
 #                       o8888888o
@@ -21,12 +21,12 @@ from src import speak,event
 #      =====`-.____`.___ \_____/___.-`___.-'=====
 #                        `=---='
 
-if __name__ == "__main__":
-    menu_options = (("About", None,event.on_about),("How", None,event.on_how))
-    systray = SysTrayIcon("./language.ico", "En_Th_OCR", menu_options,on_quit=event.on_end,)
-    systray.start()
-    speak.speak("เริ่มใช้programเเปลภาษา")
-    keyboard.add_hotkey('ctrl + shift + z', event.run)
+if __name__ == "__main__": #เป็นการประกาศให้pythonรู้ว่าตรงนี้คือส่วนการทำงานหลัก
+    menu_options = (("About", None,event.on_about),("How", None,event.on_how)) #เป็นการสร้างเมนูชื่อ Aboutกับ HowของSysTray
+    systray = SysTrayIcon("./language.ico", "En_Th_OCR", menu_options,on_quit=event.on_end,) #เป็นการสร้างSysTrayขึ้นมาชื่อEn_Th_OCRโดยตั้งiconด้วยรูป./language.ico
+    systray.start()#เริ่มการทำงานของTrayIcon
+    speak.speak("เริ่มใช้programเเปลภาษา")#ทำให้ระบบพูดว่า"เริ่มใช้programเเปลภาษา"
+    keyboard.add_hotkey('ctrl + shift + z', event.run)#เริ่มใช้hotkey ctrl + shift + z เพื่อทำงานevent.run
     
     
  
