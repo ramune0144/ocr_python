7a26bbb6afc18612d8f3a3997a40f7806767b650
Rabbit101 <70846170+ramune0144@users.noreply.github.com>
2022-01-26 13:02:15 +0700

    Update speak.py

--- src/speak.py
+++ src/speak.py
@@ -1,15 +1,13 @@
-import imp
-import re
-from gtts import gTTS
-from playsound import playsound
-import os
-def speak(dd):
-    tts = gTTS(dd,lang='th')
-    tts.save('./data/speech.mp3')
-    playsound('./data/speech.mp3')
-    os.remove('./data/speech.mp3')
+from gtts import gTTS #นำเข้าgTTSเพื่อนำมาใช้เป็นตัวกำเนิดตัวพูด
+from playsound import playsound#นำเข้าplaysoundเพื่อเป็นตัวเล่นเสียง
+import os#นำเข้าโมดูล os
+def speak(dd):#สร้างfunctionเพื่อเป็นตัวพูดจากstring
+    tts = gTTS(dd,lang='th')#กำหนดให้สร้างตัวพูดภาษาไทย
+    tts.save('./data/speech.mp3')#เซฟไฟล์ที่ตัวพูดสร้างขึ้น
+    playsound('./data/speech.mp3')#อ่านไฟล์ที่ตัวพูดสร้างขึ้น
+    os.remove('./data/speech.mp3')#ลบไฟล์ที่อ่านเเล้วออก
     try:
-        os.remove('./data/1.png')
+        os.remove('./data/1.png')#ลบรูปที่เเปลเสร็จเเล้วออก
     except:
-        pass
-    return 1
+        pass #หากผิดพลาดให้ผ่านการทำงานไปเลย
+    return 1 #ส่งกลับ1หากทำงานสำเร็จ
