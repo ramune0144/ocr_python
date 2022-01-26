0022acd710539d95bef58b57bcc2549331dffecc
Rabbit101 <70846170+ramune0144@users.noreply.github.com>
2022-01-26 12:41:34 +0700

    Update event.py

--- src/event.py
+++ src/event.py
@@ -1,13 +1,13 @@
-import ctypes
-from . import speak,ocr
-def on_end(systray):
+import ctypes #นำเข้าmoduleเพื่อสร้างwindows text box
+from . import speak,ocr #นำเข้าโมดูล speakเเละocrท
+def on_end(systray):#เป็นการสร้างfunctionชื่อว่าon_endขึ้นมาเพื่อให้พูดว่าปิดโปรเเกรม
     speak.speak("ปิดprogram")
-def on_about(systray):
+def on_about(systray):#เป็นการสร้างfunctionชื่อว่าon_aboutขึ้นม่เพื่อสร้างtextboxที่ขึ้นว่าFor Ocr EN to TH
     ctypes.windll.user32.MessageBoxW(None, u"For Ocr EN to TH", u"About", 0)   
-def on_how(systray):
+def on_how(systray):#เป็นการสร้างfunctionชื่อว่าon_howขึ้นม่เพื่อสร้างtextboxที่ขึ้นว่ากด prtscr โดยใช้โปรเเกรม Ligthshot เพื่อถ่ายหน้าจอในส่วนที่ต้องการเเปล\nจากนั้นกด 'ctrl + shift + z' เพื่อฟังเสียง
     ctypes.windll.user32.MessageBoxW(None, u"กด prtscr โดยใช้โปรเเกรม Ligthshot เพื่อถ่ายหน้าจอในส่วนที่ต้องการเเปล\nจากนั้นกด 'ctrl + shift + z' เพื่อฟังเสียง", u"วิธีการใช้งาน", 0)  
-def run():
-    try:
+def run():#เป็นการสร้างfunctionเพื่อการทำงานหลักของโปรเเกรม
+    try: #เป็นการให้โปรเเกรมลองการทำงานocr.ocr()
         ocr.ocr()
-    except:
+    except:#เป็นการบอกโปรเเกรมว่าหากทำงานไม่สำเร็จให้พูดคำว่าผิดพลาดกรุณาลองใหม่ออกมา
         speak.speak("ผิดพลาดกรุณาลองใหม่")
